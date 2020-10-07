b208592ed3a69a32cda5ae490c4b7841f6bcfc7f
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2020-10-07 17:32:41 +0900

    - Split to autoBuild and autoRun - disabled Tiered compilation tests

--- drools-benchmarks/autoBuild.sh
+++ drools-benchmarks/autoBuild.sh
@@ -2,10 +2,10 @@ cd /home/tkobayas/usr/work/DROOLS-5647/drools-OneTreeMap/drools/drools-core
 mvn clean install -DskipTests
 cd /home/tkobayas/usr/work/DROOLS-5647/kie-benchmarks/drools-benchmarks
 mvn clean install -DskipTests
-java -jar target/drools-benchmarks.jar -jvmArgs "-Xms4g -Xmx4g" -foe true -rf csv -rff results-OneTreeMap.csv -f 1 -wi 1 -i 1 org.drools.benchmarks.operators.AlphaNodeRangeIndexingBenchmark 
+cp target/drools-benchmarks.jar ./drools-benchmarks-OneTreeMap.jar
 cd /home/tkobayas/usr/work/DROOLS-5647/drools-OneRBTree/drools/drools-core
 mvn clean install -DskipTests
 cd /home/tkobayas/usr/work/DROOLS-5647/kie-benchmarks/drools-benchmarks
 mvn clean install -DskipTests
-java -jar target/drools-benchmarks.jar -jvmArgs "-Xms4g -Xmx4g" -foe true -rf csv -rff results-OneRBTree.csv -f 1 -wi 1 -i 1 org.drools.benchmarks.operators.AlphaNodeRangeIndexingBenchmark 
+cp target/drools-benchmarks.jar ./drools-benchmarks-OneRBTree.jar
 
--- drools-benchmarks/autoRun.sh
+++ drools-benchmarks/autoRun.sh
@@ -1,11 +1,4 @@
-cd /home/tkobayas/usr/work/DROOLS-5647/drools-OneTreeMap/drools/drools-core
-mvn clean install -DskipTests
 cd /home/tkobayas/usr/work/DROOLS-5647/kie-benchmarks/drools-benchmarks
-mvn clean install -DskipTests
-java -jar target/drools-benchmarks.jar -jvmArgs "-Xms4g -Xmx4g" -foe true -rf csv -rff results-OneTreeMap.csv -f 5 -wi 100000 -i 10000 org.drools.benchmarks.operators.AlphaNodeRangeIndexingBenchmark 
-cd /home/tkobayas/usr/work/DROOLS-5647/drools-OneRBTree/drools/drools-core
-mvn clean install -DskipTests
-cd /home/tkobayas/usr/work/DROOLS-5647/kie-benchmarks/drools-benchmarks
-mvn clean install -DskipTests
-java -jar target/drools-benchmarks.jar -jvmArgs "-Xms4g -Xmx4g" -foe true -rf csv -rff results-OneRBTree.csv -f 5 -wi 100000 -i 10000 org.drools.benchmarks.operators.AlphaNodeRangeIndexingBenchmark 
+java -jar ./drools-benchmarks-OneTreeMap.jar -jvmArgs "-Xms4g -Xmx4g" -foe true -rf csv -rff results-OneTreeMap.csv -f 5 -wi 100000 -i 10000 org.drools.benchmarks.operators.AlphaNodeRangeIndexingBenchmark 
+java -jar ./drools-benchmarks-OneRBTree.jar -jvmArgs "-Xms4g -Xmx4g" -foe true -rf csv -rff results-OneRBTree.csv -f 5 -wi 100000 -i 10000 org.drools.benchmarks.operators.AlphaNodeRangeIndexingBenchmark 
 
