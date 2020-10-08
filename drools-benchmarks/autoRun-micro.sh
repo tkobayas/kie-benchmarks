7f22dd8158fe85e870b346c4458e91ad4f560f6e
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2020-10-08 19:11:34 +0900

    shell for micro

--- drools-benchmarks/autoRun-micro.sh
+++ drools-benchmarks/autoRun-micro.sh
@@ -1,2 +1,2 @@
-java -jar target/drools-benchmarks.jar -jvmArgs "-Xms4g -Xmx4g" -foe true -rf csv -rff results-micro.csv -f 20 -wi 100000 -i 10000 org.drools.benchmarks.operators.TreeMapRBTreeBenchmark
+java -jar target/drools-benchmarks.jar -jvmArgs "-Xms4g -Xmx4g" -foe true -rf csv -rff results-micro.csv -f 5 -wi 100000 -i 10000 org.drools.benchmarks.operators.TreeMapRBTreeBenchmark
 
