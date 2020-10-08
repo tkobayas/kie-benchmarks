c229672dfa51f3eb76359ca6f82e04ad377ce146
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2020-10-08 18:52:31 +0900

    removed useless path

--- drools-benchmarks/autoRun-exec.sh
+++ drools-benchmarks/autoRun-exec.sh
@@ -1,4 +1,3 @@
-cd /home/tkobayas/usr/work/DROOLS-5647/kie-benchmarks/drools-benchmarks
 java -jar ./drools-benchmarks-OneTreeMap.jar -jvmArgs "-Xms4g -Xmx4g" -foe true -rf csv -rff results-OneTreeMap.csv -f 5 -wi 100000 -i 10000 org.drools.benchmarks.operators.AlphaNodeRangeIndexingExecModelBenchmark 
 java -jar ./drools-benchmarks-OneRBTree.jar -jvmArgs "-Xms4g -Xmx4g" -foe true -rf csv -rff results-OneRBTree.csv -f 5 -wi 100000 -i 10000 org.drools.benchmarks.operators.AlphaNodeRangeIndexingExecModelBenchmark 
 
