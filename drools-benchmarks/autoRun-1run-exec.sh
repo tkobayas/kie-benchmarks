9d6872a749c48f8129b5f4aa079804bb0e7baf9a
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2020-10-08 19:16:03 +0900

    minor update

--- drools-benchmarks/autoRun-1run-exec.sh
+++ drools-benchmarks/autoRun-1run-exec.sh
@@ -1,3 +1,3 @@
-java -jar ./drools-benchmarks-OneTreeMap.jar -jvmArgs "-Xms4g -Xmx4g" -foe true -rf csv -rff results-OneTreeMap.csv -f 1 -wi 1 -i 1 org.drools.benchmarks.operators.AlphaNodeRangeIndexingExecModelBenchmark 
-java -jar ./drools-benchmarks-OneRBTree.jar -jvmArgs "-Xms4g -Xmx4g" -foe true -rf csv -rff results-OneRBTree.csv -f 1 -wi 1 -i 1 org.drools.benchmarks.operators.AlphaNodeRangeIndexingExecModelBenchmark 
+java -jar ./drools-benchmarks-OneTreeMap.jar -jvmArgs "-Xms4g -Xmx4g" -foe true -rf csv -rff results-OneTreeMap-exec.csv -f 1 -wi 1 -i 1 org.drools.benchmarks.operators.AlphaNodeRangeIndexingExecModelBenchmark 
+java -jar ./drools-benchmarks-OneRBTree.jar -jvmArgs "-Xms4g -Xmx4g" -foe true -rf csv -rff results-OneRBTree-exec.csv -f 1 -wi 1 -i 1 org.drools.benchmarks.operators.AlphaNodeRangeIndexingExecModelBenchmark 
 
