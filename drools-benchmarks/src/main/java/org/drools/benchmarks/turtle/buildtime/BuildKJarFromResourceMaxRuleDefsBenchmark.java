f510b809116a1cef8d42bf4130a101c877532582
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2021-11-05 16:55:03 +0900

    MaxRuleDefsBenchmark for DROOLS-6702

---
+++ drools-benchmarks/src/main/java/org/drools/benchmarks/turtle/buildtime/BuildKJarFromResourceMaxRuleDefsBenchmark.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2018 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.benchmarks.turtle.buildtime;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.concurrent.TimeUnit;
+
+import org.drools.benchmarks.common.DRLProvider;
+import org.drools.benchmarks.common.providers.RulesWithJoinsProvider;
+import org.drools.benchmarks.common.util.BuildtimeUtil;
+import org.kie.api.KieServices;
+import org.kie.api.builder.ReleaseId;
+import org.kie.api.io.Resource;
+import org.kie.api.io.ResourceType;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+
+@State(Scope.Thread)
+@BenchmarkMode(Mode.AverageTime)
+@Warmup(iterations = 15, time = 10, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 10, timeUnit = TimeUnit.SECONDS)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+public class BuildKJarFromResourceMaxRuleDefsBenchmark {
+
+    @Param({"true", "false"})
+    private boolean useCanonicalModel;
+
+    @Param({"5000"})
+    private int numberOfRules;
+
+    @Param({"10", "100", "1000"})
+    private String maxRuleDefs;
+
+    private Resource drlResource;
+
+    @Setup
+    public void createResource() {
+        System.setProperty("drools.evaluationContext.maxRuleDefs", maxRuleDefs);
+        System.setProperty("drools.predicateInformation.maxRuleDefs", maxRuleDefs);
+
+        final DRLProvider drlProvider = new RulesWithJoinsProvider(4, false, true);
+        drlResource = KieServices.get().getResources()
+                .newReaderResource(new StringReader(drlProvider.getDrl(numberOfRules)))
+                .setResourceType(ResourceType.DRL)
+                .setSourcePath("drlFile.drl");
+    }
+
+    @Benchmark
+    public ReleaseId createKJarFromResource() throws IOException {
+        return BuildtimeUtil.createKJarFromResources(useCanonicalModel, drlResource);
+    }
+
+}
